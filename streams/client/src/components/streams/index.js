1a823194fb5b4697484700982f1dc24ca2d751b0
Okoro Ikechukwu <ikeshegs@gmail.com>
2020-12-27 21:10:47 +0100

    create boilerplate and other setup folders

---
+++ streams/client/src/components/streams/index.js
@@ -0,0 +1,13 @@
+import StreamCreate from './streamCreate';
+import StreamDelete from './streamDelete';
+import StreamEdit from './streamEdit';
+import StreamList from './streamList';
+import StreamShow from './streamShow';
+
+export {
+  StreamCreate,
+  StreamDelete,
+  StreamEdit,
+  StreamList,
+  StreamShow
+}
