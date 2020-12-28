20956c75474443c95a2d1425ec369e48e8b000e5
Okoro Ikechukwu <ikeshegs@gmail.com>
2020-12-28 10:42:31 +0100

    add google auth and login and signout functions

--- streams/client/src/components/app.js
+++ streams/client/src/components/app.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import { StreamCreate, StreamDelete, StreamEdit, StreamList, StreamShow } from './streams';
+import Header from './Header';
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
+          <Header />
           <Route path='/' exact component={StreamList} />
           <Route path='/streams/new' exact component={StreamCreate} />
           <Route path='/streams/edit' exact component={StreamEdit} />
