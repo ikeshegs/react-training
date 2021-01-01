7aba76a29dd82751eea3a4469a164d7846b54760
Okoro Ikechukwu <ikeshegs@gmail.com>
2021-01-01 16:17:50 +0100

    build streamList api

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer } from 'redux-form';
-
+import StreamReducer from './streamReducer';
 import authReducer from './authReducers';
 
 export default combineReducers({
   auth: authReducer,
   form: reducer,
+  streams: StreamReducer,
 });
